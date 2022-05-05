fbec54d2fda20fcc4ed7b0de92afac773008143b
Matt Crecraft <mcrecraft@gmail.com>
2022-05-05 16:34:10 -0500

    Added inventory to JS File

---
+++ index.js
@@ -0,0 +1,43 @@
+let inventory = [{
+    item: "+5 Dexterity Vest",
+    sell_in: 10,
+    quality: 20,
+    category: "none",
+    date_added: null
+},
+{
+    item: "Aged Brie",
+    sell_in: 2,
+    quality: 0,
+    category: "Aged Brie",
+    date_added: null
+},
+{
+    item: "Elixir of the Mongoose",
+    sell_in: 5,
+    quality: 7,
+    category: "none",
+    date_added: null
+},
+{
+    item: "Sulfuras, Hand of Ragnaros",
+    sell_in: 0,
+    quality: 80,
+    category: "Sulfuras",
+    date_added: null
+},
+{
+    item: "Backstage passes to a TAFKAL80ETC concert",
+    sell_in: 15,
+    quality: 20,
+    category: "Backstage",
+    date_added: null
+},
+{
+    item: "Conjured Mana Cake",
+    sell_in: 3,
+    quality: 8,
+    category: "Conjured",
+    date_added: null
+},
+]
