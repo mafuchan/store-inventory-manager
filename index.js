b5a082dd296d87498d45053bca75aaceefb19df2
Matt Crecraft <mcrecraft@gmail.com>
2022-05-09 20:12:40 -0500

    UPdated HTML and JS

--- index.js
+++ index.js
@@ -1,48 +1,67 @@
 const item_input = document.querySelector("#item-input")
+const stock = document.querySelector("#stock")
+const stockPile = document.querySelector(".pile")
+const date = ""
 
 let inventory = [{
     item: "+5 Dexterity Vest",
     sell_in: 10,
     quality: 20,
     category: "none",
-    date_added: null
+    date_added: today
 },
 {
     item: "Aged Brie",
     sell_in: 2,
     quality: 0,
     category: "Aged Brie",
-    date_added: null
+    date_added: today
 },
 {
     item: "Elixir of the Mongoose",
     sell_in: 5,
     quality: 7,
     category: "none",
-    date_added: null
+    date_added: today
 },
 {
     item: "Sulfuras, Hand of Ragnaros",
     sell_in: 0,
     quality: 80,
     category: "Sulfuras",
-    date_added: null
+    date_added: today
 },
 {
     item: "Backstage passes to a TAFKAL80ETC concert",
     sell_in: 15,
     quality: 20,
     category: "Backstage Passes",
-    date_added: null
+    date_added: today
 },
 {
     item: "Conjured Mana Cake",
     sell_in: 3,
     quality: 8,
     category: "Conjured",
-    date_added: null
+    date_added: today
 },
 ]
+
+input.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const item = {
+        item: formData.get("item"),
+        sell_in: formData.get("sell_in"),
+        quality: formData.get("quality"),
+        category: "none",
+        date_added: formData.get("date_added"),
+    }
+    setCategory(item)
+    qualityAssurance(item)
+    inventory = [...inventory, item]
+    return inventory
+})
 
 item_input.addEventListener("submit", (event) => {
 
